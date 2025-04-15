47a50314873d8da3a101d34752832c44f37f29c9
Ahmet Volkan Turan <a.volkan2004@gmail.com>
2025-04-15 09:00:00 -0400

    Fifth Commit: Added Comments

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -4,12 +4,20 @@
 using namespace std;
 
 
-//The time complexity of this algorithm is O()
+//The time complexity of this algorithm is O(E*Vlog(V))
+//The time complexity for the first for loop is O(V*log(V)) because it is a binary heap tree
+//In the worst case there is an update for each edge so there are E Update() calls
+//Each Update call creates a new binary heap tree so each of them have O(V*log(V)) time complexity
+//This means that the total time complexity for updating is O(E*Vlog(V))
+//The final for loop is just O(V); therefore, the total time complexity is the biggest one O(E*Vlog(V))
 int dijkstra(nodekey_t start, nodekey_t end, const Graph *g){
+
+    //Throws an invalid argument if the start or end nodes don't exist
     if(!(g->IsPresent(start) && g->IsPresent(end))){
         throw invalid_argument("The start and end nodes must exist in the graph.");
     }
 
+    //Creates a priority queue to keep track of which node to visit next
     BetterPriorityQueue pq;
     set<nodekey_t> nodes = g->GetNodes();
 
@@ -26,6 +34,7 @@ int dijkstra(nodekey_t start, nodekey_t end, const Graph *g){
 
     vector<BPQNode> result;
 
+    //Goes through the adjacent vertices of the node with the lowest priority and updates the paths to the adjacent vertices accordingly
     while(!pq.empty()){
         BPQNode curr = pq.top();
         pq.pop();
@@ -48,6 +57,7 @@ int dijkstra(nodekey_t start, nodekey_t end, const Graph *g){
 
     int distance = INT_MAX;
 
+    //Finds the distance to the end node and returns it
     for(size_t i = 0; i<result.size(); i++){
         if(result.at(i).gnode == end){
             distance = result.at(i).pri;
